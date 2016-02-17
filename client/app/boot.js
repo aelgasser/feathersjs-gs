36079b2963b435103420422bd7f5ab7778307503
Jordi Talens <talensjr@gmail.com>
2016-02-17 10:13:48 +0100

    removed *.js files for cleanup - locked angular2 peerDependencies versions

--- client/app/boot.js
+++
@@ -1,7 +0,0 @@
-var browser_1 = require('angular2/platform/browser');
-var router_1 = require('angular2/router');
-var feathers_todo_component_1 = require('./feathers-todo.component');
-browser_1.bootstrap(feathers_todo_component_1.FeathersTodoComponent, [
-    router_1.ROUTER_PROVIDERS
-]);
-//# sourceMappingURL=boot.js.map
